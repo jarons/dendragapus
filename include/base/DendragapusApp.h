e179725086b9aa838026b0dbcb1fc0e75c07289d
Jaron <jaron.senecal@gmail.com>
2015-05-28 16:32:13 -0400

    First commit

---
+++ include/base/DendragapusApp.h
@@ -0,0 +1,22 @@
+#ifndef DENDRAGAPUSAPP_H
+#define DENDRAGAPUSAPP_H
+
+#include "MooseApp.h"
+
+class DendragapusApp;
+
+template<>
+InputParameters validParams<DendragapusApp>();
+
+class DendragapusApp : public MooseApp
+{
+public:
+  DendragapusApp(const std::string & name, InputParameters parameters);
+  virtual ~DendragapusApp();
+
+  static void registerApps();
+  static void registerObjects(Factory & factory);
+  static void associateSyntax(Syntax & syntax, ActionFactory & action_factory);
+};
+
+#endif /* DENDRAGAPUSAPP_H */
